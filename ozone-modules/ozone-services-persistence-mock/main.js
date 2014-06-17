365cc5ba4128eb1f3768acc967c3216c501f7119
Kris Siegel <kris@binaryidiot.com>
2014-06-17 12:44:47 -0400

    Updated mock persistence service to be inline with the new refactored structure.

--- ozone-modules/ozone-services-persistence-mock/main.js
+++ ozone-modules/ozone-services-persistence-mock/main.js
@@ -1,11 +1,6 @@
-(function () {
+module.exports = (function (callback, Ozone) {
 	"use strict";
-
-	module.exports = function (Ozone, callback) {
-		var logger = Ozone.logger;
-		
-		require('./mock')(Ozone, function(mock) {
-			callback(mock);
-		});
-	};
-}());
+	Ozone.Service().on("ready", "PersistenceCommon", function () {
+		require("./mock")(Ozone);
+	});
+});
--- ozone-modules/ozone-services-persistence-mock/mock.js
+++ ozone-modules/ozone-services-persistence-mock/mock.js
@@ -1,32 +1,29 @@
-(function () {
+module.exports = (function (Ozone) {
     "use strict";
+    var logger = Ozone.logger,
+        collectionImp = {}, driveImp = {}, collectionHelper = require('./collectionHelper')(),
+        driveHelper = require('./driveHelper')();
+	collectionHelper.init(Ozone);
+	driveHelper.init(Ozone);
 
-    module.exports = function (Ozone, callback) {
-        var logger = Ozone.logger,
-            collectionImp = {}, driveImp = {}, collectionHelper = require('./collectionHelper')(),
-            driveHelper = require('./driveHelper')();
-		collectionHelper.init(Ozone);
-		driveHelper.init(Ozone);
+    collectionImp.get = collectionHelper.get;
+    collectionImp.query = collectionHelper.query;
+    collectionImp.set = collectionHelper.set;
+    collectionImp.remove = collectionHelper.remove;
+    collectionImp.addIndex = collectionHelper.addIndex;
+    collectionImp.getIndexes = collectionHelper.getIndexes;
+    collectionImp.removeIndex = collectionHelper.removeIndex;
+    collectionImp.removeAllIndexes = collectionHelper.removeAllIndexes;
 
-        collectionImp.get = collectionHelper.get;
-        collectionImp.query = collectionHelper.query;
-        collectionImp.set = collectionHelper.set;
-        collectionImp.remove = collectionHelper.remove;
-        collectionImp.addIndex = collectionHelper.addIndex;
-        collectionImp.getIndexes = collectionHelper.getIndexes;
-        collectionImp.removeIndex = collectionHelper.removeIndex;
-        collectionImp.removeAllIndexes = collectionHelper.removeAllIndexes;
+    driveImp.get = driveHelper.get;
+    driveImp.set = driveHelper.set;
+    driveImp.remove = driveHelper.remove;
 
-        driveImp.get = driveHelper.get;
-        driveImp.set = driveHelper.set;
-        driveImp.remove = driveHelper.remove;
+    var objectId = {
+            getObjectId: function () {
+                return Ozone.Utils.generateId();
+            }
+        };
 
-        var objectId = {
-                getObjectId: function () {
-                    return Ozone.Utils.generateId();
-                }
-            };
-        
-        callback(require('../../persistence-interface')(collectionImp, driveImp, objectId, Ozone));
-    };
-}());
+    Ozone.Service("Persistence", Ozone.Service("PersistenceCommon")(collectionImp, driveImp, objectId, Ozone));
+});
