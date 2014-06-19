b015a920ae1445ebaf31af60a72d74655d4f7eb1
Kris Siegel <kris@binaryidiot.com>
2014-06-19 00:52:20 -0400

    Documented code within the example service module.

--- ozone-modules/ozone-example-module/main.js
+++ ozone-modules/ozone-example-module/main.js
@@ -1,7 +1,19 @@
+/**
+    An example ozone service module that simply outputs text.
+
+    @module Ozone.Services.Example
+    @class Ozone.Services.Example
+    @submodule Server-Side
+*/
 (function () {
     module.exports = function (callback, Ozone) {
         var shout = require("./shouter").shout;
 
+        /**
+            Returns some text when calling the RESTful service
+
+            @method /api/example/
+        */
         Ozone.Routing.get("example/", function (req, res) {
             res.send(shout("Example module here"));
         });
