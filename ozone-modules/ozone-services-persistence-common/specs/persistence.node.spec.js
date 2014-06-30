ef2c73c7a7ffd209d4e5bc0d9a621fedde4946e8
Kris Siegel <kris@binaryidiot.com>
2014-06-30 02:00:30 -0400

    Normalized error returns to be undefined or contain a value (no longer mixes undefined and null). Added unit tests for objectId() and Metadata within Persistence. Revised personas unit tests since error normalization has occurred.

--- ozone-modules/ozone-services-persistence-common/specs/persistence.node.spec.js
+++ ozone-modules/ozone-services-persistence-common/specs/persistence.node.spec.js
@@ -5,6 +5,44 @@
 */
 module.exports = (function (Ozone) {
     describe("ozone-services-persistence", function () {
+        var store = Ozone.config().getServerProperty("persistence.store");
 
+        // This isn't ideal but works; this prevents each unit test from running until
+        // the persistence service is ready.
+        beforeEach(function (done) {
+            Ozone.Service().on("ready", "Persistence", function () {
+                done();
+            });
+        });
+
+        it("Ozone.Service('Persistence').objectId()", function () {
+            var id = Ozone.Service('Persistence').objectId();
+            expect(id).not.toBe(undefined);
+            expect(id).not.toBe(null);
+            expect(id.length).toBeGreaterThan(0);
+        });
+
+        it("Ozone.Service('Persistence').Store(store).Metadata", function (done) {
+            Ozone.Service("Persistence").Store(store).Metadata.set({
+                meta: "testing",
+                data: "127745"
+            }, function (errSet, resSet) {
+                expect(errSet).toBeUndefined();
+                Ozone.Service("Persistence").Store(store).Metadata.get("testing", function (errGet, resGet) {
+                    expect(errGet).toBeUndefined();
+                    expect(resGet.length).toBeGreaterThan(0);
+                    expect(resGet[0].meta).toBe("testing");
+                    expect(resGet[0].data).toBe("127745");
+                    Ozone.Service("Persistence").Store(store).Metadata.remove(resGet[0]._id, function (errRemove, resRemove) {
+                        expect(errRemove).toBeUndefined();
+                        done();
+                    });
+                });
+            });
+        });
+
+        it("Ozone.Service('Persistence').", function (done) {
+            done();
+        });
     });
 });
