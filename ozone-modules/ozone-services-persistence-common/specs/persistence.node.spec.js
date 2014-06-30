0bd54ee48f46b0345b27b056657563ca2fec25e2
Kris Siegel <kris@binaryidiot.com>
2014-06-30 02:17:18 -0400

    Covered persistence service that deals with data collections with unit tests.

--- ozone-modules/ozone-services-persistence-common/specs/persistence.node.spec.js
+++ ozone-modules/ozone-services-persistence-common/specs/persistence.node.spec.js
@@ -17,8 +17,8 @@ module.exports = (function (Ozone) {
 
         it("Ozone.Service('Persistence').objectId()", function () {
             var id = Ozone.Service('Persistence').objectId();
-            expect(id).not.toBe(undefined);
-            expect(id).not.toBe(null);
+            expect(id).not.toBeUndefined();
+            expect(id).not.toBeNull();
             expect(id.length).toBeGreaterThan(0);
         });
 
@@ -41,8 +41,84 @@ module.exports = (function (Ozone) {
             });
         });
 
-        it("Ozone.Service('Persistence').", function (done) {
-            done();
+        it("Ozone.Service('Persistence').Store(store).Collection(collection).set()", function (done) {
+            Ozone.Service('Persistence').Store(store).Collection("TestCollection").set(Ozone.Service("Persistence").objectId(), {
+                testObj: "ftw"
+            }, function (err, res) {
+                expect(err).toBeUndefined();
+                done();
+            });
         });
+
+        it("Ozone.Service('Persistence').Store(store).Collection(collection).get()", function (done) {
+            var id = Ozone.Service("Persistence").objectId();
+            Ozone.Service('Persistence').Store(store).Collection("TestCollection").set(id, {
+                testObj: "ftw"
+            }, function (err, res) {
+                expect(err).toBeUndefined();
+                Ozone.Service('Persistence').Store(store).Collection("TestCollection").get(id, function (errGet, resGet) {
+                    expect(errGet).toBeUndefined();
+                    expect(resGet).not.toBeUndefined();
+                    expect(resGet.length).toBeGreaterThan(0);
+                    expect(resGet[0].testObj).toBe("ftw");
+                    done();
+                });
+            });
+        });
+
+        it("Ozone.Service('Persistence').Store(store).Collection(collection).remove()", function (done) {
+            var id = Ozone.Service("Persistence").objectId();
+            Ozone.Service('Persistence').Store(store).Collection("TestCollection").set(id, {
+                testObj: "ftw"
+            }, function (err, res) {
+                expect(err).toBeUndefined();
+                Ozone.Service('Persistence').Store(store).Collection("TestCollection").remove(id, function (errRem, resRes) {
+                    expect(err).toBeUndefined();
+                    done();
+                });
+            });
+        });
+
+        it("Ozone.Service('Persistence').Store(store).Collection(collection).query()", function (done) {
+            Ozone.Service('Persistence').Store(store).Collection("TestCollectionQuery").set(Ozone.Service("Persistence").objectId(), {
+                name: "Bob"
+            }, function (err1, res1) {
+                expect(err1).toBeUndefined();
+                Ozone.Service('Persistence').Store(store).Collection("TestCollectionQuery").set(Ozone.Service("Persistence").objectId(), {
+                    name: "Kris"
+                }, function (err2, res2) {
+                    expect(err2).toBeUndefined();
+                    Ozone.Service('Persistence').Store(store).Collection("TestCollectionQuery").set(Ozone.Service("Persistence").objectId(), {
+                        name: "Robert"
+                    }, function (err3, res3) {
+                        expect(err3).toBeUndefined();
+                        Ozone.Service("Persistence").Store(store).Collection("TestCollectionQuery").query({ name: "Kris" }, undefined, function (errQuery, resQuery) {
+                            expect(errQuery).toBeUndefined();
+                            expect(resQuery).not.toBeUndefined();
+                            expect(resQuery.length).toBeGreaterThan(0);
+                            expect(resQuery[0].name).toBe("Kris");
+                            done();
+                        });
+                    });
+                });
+            });
+        });
+
+        /*
+        it("Ozone.Service('Persistence').", function (done) {
+
+        });
+
+        it("Ozone.Service('Persistence').", function (done) {
+
+        });
+
+        it("Ozone.Service('Persistence').", function (done) {
+
+        });
+
+        it("Ozone.Service('Persistence').", function (done) {
+
+        });*/
     });
 });
