1eeaa47388e1a75f8a122da270569e3305aa158b
natebynum <nathan.bynum@axiosengineering.com>
2014-07-24 07:30:21 -0400

    Roles are empty. Try importing data before calling export so roles are populated

--- ozone-modules/ozone-services-personas/specs/personas.node.spec.js
+++ ozone-modules/ozone-services-personas/specs/personas.node.spec.js
@@ -147,7 +147,12 @@ module.exports = (function (Ozone) {
         });
 
         it('exports all of the persona.persona data', function(done){
-            personaService.export(function(data){
+        	var dataSet = require('./spec-data/testImport.json');
+            personaService.import(dataSet, function(){
+                //personaService.persona.query({username: "testSystemAdmin1"}, function(err, result){});
+            });
+        	
+        	personaService.export(function(data){
                 expect(data['Personas'].length).toBe(6);
                 var person = find(data['Personas'], 'username', 'testOzoneAdmin1');
                 expect(person).not.toBeUndefined();
